4c9b8d59a00d813f968755befdc42f0ceaf15871
RoseVar <65072255+RoseVar@users.noreply.github.com>
2020-05-12 12:03:30 +0200

    Update LoginActivity.java
    Intento arreglar para que no se pegue

--- app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
+++ app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.petpal.petpaltravel.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
-import com.petpal.petpaltravel.R;
+        import com.petpal.petpaltravel.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +12,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
-
     }
+
 }

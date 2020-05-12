0d2b2bea6280792460e20bbaf33b5da6604ee397
mgarcang <65112669+mgarcang@users.noreply.github.com>
2020-05-12 11:57:35 +0200

    shelterdemand layout
    Shelterdemand in progress...

--- app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
+++ app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
@@ -1,19 +1,46 @@
 package com.petpal.petpaltravel.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.petpal.petpaltravel.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewaccount_layout);
+        setContentView(R.layout.login_layout);
+
+        /*final EditText etFechadisponible = (EditText) findViewById(R.id.etFechadisponible);
+        etFechadisponible.setOnClickListener((View.OnClickListener) this);
+
+        etFechadisponible.setOnClickListener(new View.OnClickListener() { 
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+
+            // Método para la fecha disponible del layout shelterdemand
+            private void showDatePickerDialog() {
+                DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        // +1 because January is zero
+                        final String selectedDate = day + " / " + (month+1) + " / " + year;
+                        etFechadisponible.setText(selectedDate);
+                    }
+                });
+
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });*/
     }
 
     // Método para mostrar y ocultar el menú

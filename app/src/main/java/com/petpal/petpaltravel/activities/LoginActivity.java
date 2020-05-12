89750136a9ed5e600837b1358ad46498f1ce4e57
mgarcang <65112669+mgarcang@users.noreply.github.com>
2020-05-12 08:57:03 +0200

    viewaccount_layout.xml
    Layout para visualizar los datos de registro finalizado.

--- app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
+++ app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
@@ -13,7 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.account_layout);
+        setContentView(R.layout.viewaccount_layout);
     }
 
     // Método para mostrar y ocultar el menú

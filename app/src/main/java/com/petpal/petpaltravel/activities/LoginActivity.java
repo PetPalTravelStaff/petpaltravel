239ed705c625a7caca861998bd71db5314ee5767
mgarcang <65112669+mgarcang@users.noreply.github.com>
2020-05-13 11:56:39 +0200

    Personoffer_layout
    Personoffer_layout finalizado.
    Pendiente de calendario y mapa.

--- app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
+++ app/src/main/java/com/petpal/petpaltravel/activities/LoginActivity.java
@@ -17,7 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewdetailsdemand_layout);
+        setContentView(R.layout.login_layout);
 
     }
 

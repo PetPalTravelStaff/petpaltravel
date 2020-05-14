00bb68095e2ea63eb455ff58a935c0741393e72c
RoseVar <65072255+RoseVar@users.noreply.github.com>
2020-05-14 20:02:17 +0200

    Seach Demands y Offers acabados
    SearchOffers y SearchDemands acabados y encarriladas otras cosas

---
+++ app/src/main/java/com/petpal/petpaltravel/activities/ShowMyOffersActivity.java
@@ -0,0 +1,13 @@
+package com.petpal.petpaltravel.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMyOffersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
